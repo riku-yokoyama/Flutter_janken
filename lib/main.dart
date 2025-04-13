b9051ae6171951759968ebf1246b47e9ffa35448
DairikuYoko <yokoriku@gmail.com>
2025-04-13 21:40:36 +0900

    ロボットの手を画像で表示する

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'じゃんけん'),
     );
   }
 }
@@ -31,15 +30,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ロボットの出す手を決定する
+  String chooseHand() {
+    List<String> hands = ["グー", "チョキ", "パー"];
+    hands.shuffle(); // シャッフル
+    String hand = hands.first;
+    String result = "";
+
+    if (hand == "グー") {
+      result = "assets/janken_gu.png";
+    } else if (hand == "チョキ") {
+      result = "assets/janken_choki.png";
+    } else {
+      result = "assets/janken_pa.png";
+    }
+
+    return result;
+  }
+
+  // じゃんけん：ロボットの手を表示します
+  void showRobotResult() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text(
+              "ロボットくん",
+              style: TextStyle(fontSize: 20),
+            ),
+            content: Image.asset(chooseHand()),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("閉じる"))
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              showRobotResult();
+            },
+            child: const Text("じゃんけん"),
+          ),
+        ));
   }
 }
